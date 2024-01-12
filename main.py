5ceab833a57264dcc025182e79aca90b846b0955
Onyx <shadowangel404@proton.me>
2024-01-12 21:22:19 +0100

    Mise à jour pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-def verify():
-    end: game.LedSprite = None
-    sprite: game.LedSprite = None
-    if [sprite.get(LedSpriteProperty.X),
-        sprite.get(LedSpriteProperty.Y)] == [end.get(LedSpriteProperty.X), end.get(LedSpriteProperty.Y)]:
-        return 1
-    else:
-        pass
+def move():
+    if input.rotation(Rotation.PITCH) < -20 and False:
+        sprite.change(LedSpriteProperty.X, 1)
+    elif input.rotation(Rotation.PITCH) > 20 and False:
+        sprite.change(LedSpriteProperty.X, -1)
+    elif input.rotation(Rotation.ROLL) < -20 and False:
+        sprite.change(LedSpriteProperty.Y, 1)
+    elif input.rotation(Rotation.ROLL) > 20 and False:
+        sprite.change(LedSpriteProperty.Y, -1)
+sprite: game.LedSprite = None
+sprite = game.create_sprite(4, 4)
+
+def on_forever():
+    move()
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,11 +1,35 @@
-function verify(): number {
-    let end : game.LedSprite = null
-    let sprite : game.LedSprite = null
-    if ([sprite.get(LedSpriteProperty.X), sprite.get(LedSpriteProperty.Y)] == [end.get(LedSpriteProperty.X), end.get(LedSpriteProperty.Y)]) {
-        return 1
-    } else {
-        
-    }
-    
+function level () {
+	
 }
-
+function move () {
+    if (input.rotation(Rotation.Pitch) < -20) {
+        sprite.change(LedSpriteProperty.Y, -1)
+    } else if (input.rotation(Rotation.Pitch) > 20) {
+        sprite.change(LedSpriteProperty.Y, 1)
+    } else if (input.rotation(Rotation.Roll) < -20) {
+        sprite.change(LedSpriteProperty.X, -1)
+    } else if (input.rotation(Rotation.Roll) > 20) {
+        sprite.change(LedSpriteProperty.X, 1)
+    }
+    sprite.change(LedSpriteProperty.Brightness, 0.5)
+    basic.pause(200)
+}
+function verify (list: number[][]) {
+    if (sprite.isTouching(end)) {
+        game.addScore(1)
+        level()
+    } else if (list.indexOf([sprite.get(LedSpriteProperty.X), sprite.get(LedSpriteProperty.Y)]) == -1) {
+        game.gameOver()
+    }
+}
+let end: game.LedSprite = null
+let sprite: game.LedSprite = null
+sprite = game.createSprite(4, 4)
+end = game.createSprite(0, 0)
+end.set(LedSpriteProperty.Brightness, 100)
+game.setScore(0)
+basic.forever(function () {
+    let list: number[] = []
+    move()
+    verify(list)
+})
