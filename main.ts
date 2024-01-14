2c49ba3b0cd42455cad993ad41617b7b44f1ab74
Onyx <shadowangel404@proton.me>
2024-01-14 11:28:40 +0100

    Mise à jour main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-function level () {
-	
-}
 function move () {
     if (input.rotation(Rotation.Pitch) < -20) {
         sprite.change(LedSpriteProperty.Y, -1)
@@ -14,22 +11,47 @@ function move () {
     sprite.change(LedSpriteProperty.Brightness, 0.5)
     basic.pause(200)
 }
-function verify (list: number[][]) {
+function verify (list2: any[]) {
     if (sprite.isTouching(end)) {
-        game.addScore(1)
-        level()
-    } else if (list.indexOf([sprite.get(LedSpriteProperty.X), sprite.get(LedSpriteProperty.Y)]) == -1) {
-        game.gameOver()
+        return 1
+    } else if (obstacles.indexOf([sprite.get(LedSpriteProperty.X), sprite.get(LedSpriteProperty.Y)]) >= 0) {
+        return 2
+    } else {
+        return 0
     }
 }
+let dead = 0
+let obstacles: number[][] = []
 let end: game.LedSprite = null
 let sprite: game.LedSprite = null
+let lpos : number[][] = []
 sprite = game.createSprite(4, 4)
 end = game.createSprite(0, 0)
-end.set(LedSpriteProperty.Brightness, 100)
 game.setScore(0)
+for (let i = 0; i <= 4; i++) {
+    for (let j = 0; j <= 4; j++) {
+        lpos.push([i, j])
+    }
+}
+lpos.shift()
+_py.py_array_pop(lpos)
 basic.forever(function () {
-    let list: number[] = []
-    move()
-    verify(list)
+    basic.clearScreen()
+    obstacles = []
+    for (let index = 0; index < Math.idiv(game.score(), 2) + 1; index++) {
+        obstacles.push(lpos[randint(0, lpos.length - 1)])
+    }
+    dead = 0
+    while (!(dead)) {
+        for (let valeur of obstacles) {
+            led.plot(valeur[0], valeur[1])
+        }
+        move()
+        dead = verify(obstacles)
+    }
+    if (dead == 1) {
+        game.addScore(1)
+    } else if (dead == 2) {
+        game.gameOver()
+    }
 })
